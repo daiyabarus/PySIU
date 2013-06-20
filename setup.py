fff899a211de245be598b1fdeea1fa46b34f0df3
Esteban Garcia-Gurtubay <esteban.garcia.gurtubay@gmail.com>
2013-06-20 02:10:45 +0200

    First commit

--- setup.py
+++ setup.py
@@ -8,18 +8,14 @@ __email__ = 'esteban.garcia.gurtubay@gmail.com'
 __date__ = '19/06/2013 17:07:15'
 
 
-#from distutils.core import setup               # To make a pip .tar.gz
-from setuptools import setup, find_packages     # To make an easy_install .egg
+from distutils.core import setup               # To make a pip .tar.gz
 
 setup(
-    name='pysiu',
+    name='PySIU',
     version = __version__,
     license='LICENSE.txt',
     author=__author__,
     author_email=__email__,
     description='Python utility libraries for the Ericsson SIU nodes',
-    #packages=find_packages(exclude=['other', '*.test']),
-    packages=find_packages(exclude=['other',]),
-    package_data={'pysiu': ['*.sh'],
-                },
+    packages=['pysiu'],
 )
